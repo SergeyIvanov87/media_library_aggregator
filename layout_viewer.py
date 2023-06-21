77c8581b8e57d9c4b083b0bda11855c84a33839a
SergeyIvanov87 <sergius140608@rambler.ru>
2023-06-21 23:08:11 +0200

    Apply style & pylint comments

--- layout_viewer.py
+++ layout_viewer.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python
-
+"""
+The module builds GUI instance from a layout representation.
+It parses the layout data and fills respective grid of Widgets accordingly to the layout representation.
+"""
 import json
-from playsound import playsound
 import multiprocessing
-
 import tkinter
+
 from tkinter import ttk
+from playsound import playsound
 from PIL import Image, ImageTk
 
 import layout_loader
 from collector_utils import file_name_to_object_name
 
-"""
-TODO playsound module required
-"""
 class PlaysoundCtrl:
+    """
+    TODO playsound module required
+    """
     playsound_process = None
     last_client_stop_callback = None
     last_client_id = None
@@ -23,17 +26,18 @@ class PlaysoundCtrl:
         if self.playsound_process:
             self.playsound_process.terminate()
 
-        if not self.last_client_id  == client_id:
+        if not self.last_client_id == client_id:
             if self.last_client_stop_callback:
                 self.last_client_stop_callback()
 
-            self.last_client_stop_callback = None;
+            self.last_client_stop_callback = None
             self.last_client_id = None
-
 
     def start(self, sound_file, client_id, client_callback):
         if sound_file is not None:
-            self.playsound_process = multiprocessing.Process(target=playsound, args=(sound_file,))
+            self.playsound_process = multiprocessing.Process(
+                target=playsound, args=(sound_file,)
+            )
             self.playsound_process.start()
             self.last_client_stop_callback = client_callback
             self.last_client_id = client_id
@@ -67,8 +71,10 @@ class ObjectRepresentative(tkinter.ttk.Button):
         self.photo_images.append(next_image)
         return next_image
 
+
 class DescriptionRepresentative(tkinter.Toplevel):
     info_labels = []
+
     def __init__(self, info_files_list):
         tkinter.Toplevel.__init__(self)
         self.grid_rowconfigure(0, weight=1)
@@ -77,14 +83,16 @@ class DescriptionRepresentative(tkinter.Toplevel):
         self.info_book = ttk.Notebook(self)
         self.info_book.grid_rowconfigure(0, weight=1)
         self.info_book.grid_columnconfigure(0, weight=1)
-        self.info_book.pack(fill='both', expand=True)
+        self.info_book.pack(fill="both", expand=True)
 
-        self.info_labels = [tkinter.Label(self.info_book, text=info) for info in info_files_list]
+        self.info_labels = [
+            tkinter.Label(self.info_book, text=info) for info in info_files_list
+        ]
         for i in self.info_labels:
             i.grid_rowconfigure(0, weight=1)
             i.grid_columnconfigure(0, weight=0)
             i.grid_columnconfigure(1, weight=1)
-            i.pack(fill='both', expand=True)
+            i.pack(fill="both", expand=True)
 
             self.info_book.add(i)
 
@@ -119,21 +127,27 @@ class ObjectFrame(ttk.Frame):
     sound_files_list = []
     parent_sound_controller = None
 
-    def __init__(self, parent, sound_controller, dimensions, description_list, images_list, sounds_list):
+    def __init__(
+        self,
+        parent,
+        sound_controller,
+        dimensions,
+        object_attr_list
+    ):
         ttk.Frame.__init__(self, parent)
 
         self.parent_sound_controller = sound_controller
 
-        self.info_list = description_list
-        self.sound_files_list = sounds_list
+        self.info_list = object_attr_list[0]
+        self.sound_files_list = object_attr_list[2]
         if len(self.sound_files_list):
-            self.sound_files_list.append(None) #stop play sound marker
+            self.sound_files_list.append(None)  # stop play sound marker
         print(self.sound_files_list)
-        self.object_view = ObjectRepresentative(self, dimensions, images_list)
+        self.object_view = ObjectRepresentative(self, dimensions, object_attr_list[1])
         self.object_view.grid(row=0, column=0)
 
         self.controls_frame = ttk.Frame(self)
-        self.controls_frame.grid(row=1,column=0)
+        self.controls_frame.grid(row=1, column=0)
 
         # always create 'Description' Label
         object_name = "UNKNOWN"
@@ -142,26 +156,34 @@ class ObjectFrame(ttk.Frame):
             object_name = file_name_to_object_name(self.info_list[0])
 
         print(object_name)
-        self.description_label = tkinter.ttk.Label(self.controls_frame, text=object_name)
-        self.description_label.grid(row=0, column=control_frame_widget_column_index,
-                                    padx = 5, pady = 5, ipadx = 5, ipady = 5,\
-                                    sticky='w')
+        self.description_label = tkinter.ttk.Label(
+            self.controls_frame, text=object_name
+        )
+        self.description_label.grid(
+            row=0,
+            column=control_frame_widget_column_index,
+            padx=5,
+            pady=5,
+            ipadx=5,
+            ipady=5,
+            sticky="w",
+        )
         control_frame_widget_column_index = control_frame_widget_column_index + 1
 
         # create 'Show Info' button if necessary
         if len(self.info_list):
-            self.description_show_button = tkinter.ttk.Button(self.controls_frame,
-                text="Show Info",
-                command=self.on_description_click
+            self.description_show_button = tkinter.ttk.Button(
+                self.controls_frame, text="Show Info", command=self.on_description_click
             )
-            self.description_show_button.grid(row=0, column=control_frame_widget_column_index)
+            self.description_show_button.grid(
+                row=0, column=control_frame_widget_column_index
+            )
             control_frame_widget_column_index = control_frame_widget_column_index + 1
 
         # create 'Play Sound' button if necessary
         if len(self.sound_files_list):
-            self.play_sound_button = tkinter.ttk.Button(self.controls_frame,
-                text="Play",
-                command=self.on_play_sound_click
+            self.play_sound_button = tkinter.ttk.Button(
+                self.controls_frame, text="Play", command=self.on_play_sound_click
             )
             self.play_sound_button.grid(row=0, column=control_frame_widget_column_index)
             control_frame_widget_column_index = control_frame_widget_column_index + 1
@@ -177,7 +199,9 @@ class ObjectFrame(ttk.Frame):
         next_song = self.__extract_next_sound__()
         if next_song is not None:
             # start new sound and register own instance as client_id
-            self.parent_sound_controller.start(next_song, self, self.rewind_sound_list_to_beginning)
+            self.parent_sound_controller.start(
+                next_song, self, self.rewind_sound_list_to_beginning
+            )
 
     def rewind_sound_list_to_beginning(self):
         """
@@ -198,9 +222,13 @@ class ObjectFrame(ttk.Frame):
 
         # change sound button caption
         if next_sound is not None:
-            self.__change_sound_button_caption("Playing..." if self.__is_next_sound__() else "Stop")
+            self.__change_sound_button_caption(
+                "Playing..." if self.__is_next_sound__() else "Stop"
+            )
         else:
-            self.__change_sound_button_caption("Play" if self.__is_next_sound__() else "Stop")
+            self.__change_sound_button_caption(
+                "Play" if self.__is_next_sound__() else "Stop"
+            )
         return next_sound
 
     def __is_next_sound__(self):
@@ -209,15 +237,13 @@ class ObjectFrame(ttk.Frame):
     def __change_sound_button_caption(self, caption):
         self.play_sound_button.config(text=caption)
 
+
 class MainLayoutWidget(tkinter.Tk):
     object_cells = []
     sound_ctrl = PlaysoundCtrl()
 
     def __init__(self, json_layout):
         tkinter.Tk.__init__(self)
-
-        name = json_layout["name"]
-        self.title(f"Layout: {name}")
 
         main_frame = ttk.Frame(
             self,
@@ -228,10 +254,11 @@ class MainLayoutWidget(tkinter.Tk):
 
         rows = json_layout["layout"]["rows"]
         columns = json_layout["layout"]["columns"]
+        self.title(f"Layout: {json_layout['name']} {rows}x{columns}")
 
         cell_width = int(main_frame.winfo_reqwidth() / rows)
         cell_height = int(main_frame.winfo_reqheight() / columns)
-        print(f"column: {columns}, rows: {rows}")
+
         cells = json_layout["mapping"]["cells"]
         cells_column = [json.loads("{}") for c in range(0, columns)]
         cells_matrix = [list(cells_column) for r in range(0, rows)]
@@ -252,14 +279,12 @@ class MainLayoutWidget(tkinter.Tk):
                         main_frame,
                         self.sound_ctrl,
                         (cell_width, cell_height),
-                        cell["o"],
-                        cell["i"],
-                        cell["s"],
+                        (cell["o"], cell["i"], cell["s"])
                     )
                 )
                 self.object_cells[r][c].grid(
                     row=r, column=c
-                )  # , sticky ='w'+'n'+'e'+'s')
+                )
 
 
 layout_name = input("Please specify a layout name: ")
